9d37c6d61eb467fb50db741adb6af912294da74e
shikoliamos31 <amosshikoli@gmail.com>
2024-03-20 07:31:14 -0700

    States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -1,60 +1,63 @@
 #!/usr/bin/python3
-"""
-This module defines the BaseModel class.
-"""
-import uuid
-from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+import models
+
+Base = declarative_base()
 
 
 class BaseModel:
+    """This class will define all common attributes/methods
+    for other classes
     """
-    BaseModel class that defines all common attributes/methods for other classes.
-    """
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """
-        Initializes a new instance of BaseModel.
-
+        """Instantiation of base model class
         Args:
-            *args: Variable length argument list.
-            **kwargs: Arbitrary keyword arguments.
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
         """
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif key != '__class__':
+                if key != '__class__':
                     setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.utcnow()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.utcnow()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.utcnow()
 
     def save(self):
+        """updates the public instance attribute updated_at to current
         """
-        Updates the public instance attribute updated_at with the current datetime.
-        """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
+        """creates dictionary of the class  and returns
+        Return:
+            returns a dictionary of all the key values in __dict__
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance.
+        my_dict = dict(self.__dict__)
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict.pop('_sa_instance_state', None)  # Remove SQLAlchemy state
+        return my_dict
 
-        Returns:
-            dict: Dictionary containing the object attributes.
+    def delete(self):
+        """delete object
         """
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
-
-    def __str__(self):
-        """
-        Returns the string representation of the object.
-
-        Returns:
-            str: String representation of the object.
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
+        models.storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """This module contains the City class."""
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
+from models.place import Place  # Add this import statement
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """City class that inherits from BaseModel."""
 
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
 
     def __init__(self, *args, **kwargs):
         """Initialize City class."""
         super().__init__(*args, **kwargs)
-
