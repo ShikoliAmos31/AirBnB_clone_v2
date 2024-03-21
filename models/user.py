73b2212a8c2db7f7b6f6ddbe1a309e06e4a9208a
Locozyoungg <collaustin27@gmail.com>
2024-03-20 17:43:19 -0700

    user_update

--- models/user.py
+++ models/user.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
@@ -12,7 +11,7 @@ class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
-        password: password for you login
+        password: password for your login
         first_name: first name
         last_name: last name
     """
@@ -20,8 +19,8 @@ class User(BaseModel, Base):
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
     places = relationship("Place", cascade='all, delete, delete-orphan',
                           backref="user")
