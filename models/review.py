8997fa0abf376115a06eb9191c106f948c1b9188
shikoliamos31 <amosshikoli@gmail.com>
2024-03-20 19:11:23 -0700

    DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """This module contains the Review class."""
-from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
-class Review(BaseModel):
-    """Review class that inherits from BaseModel."""
-
-    place_id = ""
-    user_id = ""
-    text = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initialize Review class."""
-        super().__init__(*args, **kwargs)
-
+class Review(BaseModel, Base):
+    """This is the class for Review
+    Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
